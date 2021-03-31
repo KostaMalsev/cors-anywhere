f3b9f6420f95aeda8f09a1270ab436c02a1a17a4
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-03-31 14:12:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 8080;
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
 //var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
-var originWhitelist = ['https://loocat.netlify.app','https://dnav.netlify.app/'];
+var originWhitelist = ['https://loocat.netlify.app','https://dnav.netlify.app'];
 
 function parseEnvList(env) {
   if (!env) {
